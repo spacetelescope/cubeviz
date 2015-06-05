1171152bb8bec90c90dab18bf63bbe4c05b9b527
nmearl <nchlsearl@gmail.com>
2015-06-05 15:12:23 -0400

    Got rid of some build cruft in the setup.py file

--- setup.py
+++ setup.py
@@ -3,9 +3,7 @@ from distutils.core import setup
 setup(
     name='Cube Tools',
     version='0.0.1',
-    packages=['build.lib.cube_tools', 'build.lib.cube_tools.qt',
-              'build.lib.cube_tools.core', 'build.lib.cube_tools.clients',
-              'cube_tools', 'cube_tools.qt', 'cube_tools.core',
+    packages=['cube_tools', 'cube_tools.qt', 'cube_tools.core',
               'cube_tools.clients'],
     url='https://github.com/spacetelescope/cube-tools',
     license='',
