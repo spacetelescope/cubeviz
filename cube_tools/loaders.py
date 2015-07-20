95f102e82b1e3b02110bfac02fd6d771e29bcbfb
J.D.Eisenhamer <eisenhamer@stsci.edu>
2015-07-20 15:48:12 -0400

    Fixed glue wcs loading from the cube.

--- cube_tools/loaders.py
+++ cube_tools/loaders.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 from glue.core import Data, Component
 from glue.config import data_factory
 from glue.core.data_factories.helpers import has_extension
+from glue.core.coordinates import WCSCoordinates
 
 from cube_tools.core.data_objects import CubeData
 
@@ -13,6 +14,6 @@ def read_cube(filename, **kwargs):
 
     data = Data()
     data.add_component(Component(cube_data), label="cube")
-    print("Loaded successfully")
+    data.coords = WCSCoordinates('', wcs=cube_data.wcs)
 
     return data
