0ed1c59d581eb3fa8e6ae6f3c4f68240dcec74ed
Dan D'Avella <drdavella@gmail.com>
2017-09-12 14:32:55 -0400

    Add stub for glue plugin entry point

--- setup.py
+++ setup.py
@@ -98,7 +98,10 @@ package_info['package_data'].setdefault(PACKAGENAME, [])
 package_info['package_data'][PACKAGENAME].append('data/*')
 
 # Define entry points for command-line scripts
-entry_points = {'gui_scripts': ['cubeviz=cubeviz.cubeviz:main']}
+entry_points = {
+    'gui_scripts': ['cubeviz=cubeviz.cubeviz:main'],
+    'glue.plugins': ['cubeviz=cubeviz.cubeviz:setup']
+}
 
 if conf.has_section('entry_points'):
     entry_point_list = conf.items('entry_points')
