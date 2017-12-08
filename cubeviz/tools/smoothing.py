aa8d82b93ded0bd9c69e69693061b9fe7b3b5ab6
Robel Geda <rgeda@stsci.edu>
2017-12-08 16:20:42 -0500

    Comments and median fix

--- cubeviz/tools/smoothing.py
+++ cubeviz/tools/smoothing.py
@@ -22,18 +22,32 @@ class Smooth(object):
 
     def __init__(self, data=None, smoothing_axis=None, kernel_type=None, kernel_size=None,
                  component_id=None, output_label=None, output_as_component=False, parent=None):
-        self.data = data
-        self.smoothing_axis = smoothing_axis
-        self.kernel_type = kernel_type
-        self.kernel_size = kernel_size
-        self.component_id = component_id
-        self.output_label = output_label
-        self.output_as_component = output_as_component
-        self.kernel_registry = self.load_kernel_registry()
-        self.parent = parent
+        self.data = data  # Glue data to be smoothed
+        self.smoothing_axis = smoothing_axis  # spectral vs spatial
+        self.kernel_type = kernel_type  # Type of kernel, a key in kernel_registry
+        self.kernel_size = kernel_size  # Size of kernel in pix
+        self.component_id = component_id  # Glue data component to smooth over
+        self.output_label = output_label  # Output label
+        self.output_as_component = output_as_component  # Add output component to self.data
+        self.kernel_registry = self.load_kernel_registry()  # A list of kernels and params
+        self.parent = parent  # If gui is going to be used, parent of new gui
 
     @staticmethod
     def load_kernel_registry():
+        """
+        Nested dict to store available kernel options.
+
+        kernel_registry: Main registry
+            kernel_type: Type of kernel
+                name: Display name
+                unit_label: Display units of kernel size
+                size_prompt: User prompt for size of kernel
+                axis: Available axes. Each must have a kernel.
+                spatial: 2D astropy.convolution.kernel else None.
+                spectral: 1D astropy.convolution.kernel else None.
+
+        :return: kernel_registry
+        """
         kernel_registry = {
             "box": {"name": "Box",
                     "unit_label": "Pixels",
@@ -86,9 +100,14 @@ class Smooth(object):
         return self.kernel_registry
     
     def get_kernel(self):
+        """
+        Gets an kernel using the saved parameters
+        :return: astropy.convolution.kernel
+        """
         return self.kernel_registry[self.kernel_type][self.smoothing_axis](self.kernel_size)
     
     def get_kernel_size_prompt(self, kernel_type=None):
+        """kernel_type -> size_prompt"""
         if kernel_type is None:
             if self.kernel_type is None:
                 return None
@@ -98,6 +117,7 @@ class Smooth(object):
         return self.kernel_registry[kernel_type]["size_prompt"]
 
     def get_kernel_unit(self, kernel_type=None):
+        """kernel_type -> unit_label"""
         if kernel_type is None:
             if self.kernel_type is None:
                 return None
@@ -107,18 +127,25 @@ class Smooth(object):
         return self.kernel_registry[kernel_type]["unit_label"]
 
     def name_to_kernel_type(self, name):
+        """kernel name -> kernel_type"""
         for k in self.kernel_registry:
             if name == self.kernel_registry[k]["name"]:
                 return k
         return None
                 
     def get_glue_wcs(self):
+        """
+        Get WCS of glue Data.
+        :return: astropy.wcs.WCS
+        :raises: Exception WCS not found
+        """
         coords = self.data.coords
         if isinstance(coords, WCSCoordinates):
             return coords.wcs
         raise Exception("WCS information was not provided.")
 
     def get_glue_mask(self):
+        """Function make/extract mask of glue Data"""
         if isinstance(self.data, Subset):
             try:
                 mask = self.data.to_mask()
@@ -132,6 +159,7 @@ class Smooth(object):
         return mask
 
     def data_to_cube(self):
+        """Glue Data -> SpectralCube"""
         if self.component_id is None:
             raise Exception("component_id was not provided.")
         wcs = self.get_glue_wcs()
@@ -145,6 +173,18 @@ class Smooth(object):
     def cube_to_data(self, cube,
                      output_label=None,
                      output_component_id=None):
+        """
+        Convert SpectralCube to final output.
+        self.output_as_component is checked here.
+        if self.output_as_component:
+            add new component to self.data
+        else:
+            create new data and return it.
+        :param cube: SpectralCube
+        :param output_label: Name of new Data.
+        :param output_component_id: label of new component
+        :return:
+        """
         original_data = self.data
         if self.output_as_component:
             original_data.add_component(cube._data.copy(), output_component_id)
@@ -156,9 +196,18 @@ class Smooth(object):
             return new_data
 
     def smooth_cube(self):
+        """
+        Main smoothing function that follows the following steps:
+        1) Convert data to SpectralCube
+        2) Obtain Kernel using saved parameters (if applicable)
+        3) Smooth cube
+        4) Generate name based on saved parameters
+        5) Output component or Data
+        :return: glue.core.Data or None
+        """
         cube = self.data_to_cube()
 
-        if "Median" == self.kernel_type:
+        if "median" == self.kernel_type:
             if self.smoothing_axis == "spatial":
                 new_cube = cube.spatial_smooth_median(self.kernel_size)
             else:
@@ -192,10 +241,15 @@ class Smooth(object):
         return output
 
     def gui(self):
+        """Call smoothing gui and add output as component"""
         ex = SelectSmoothing(self.data, self.parent)
 
 
 class SelectSmoothing(QMainWindow):
+    """
+    SelectSmoothing launches a GUI and executes smoothing.
+    Any output is added to the input data as a new component.
+    """
 
     def __init__(self, data, parent=None, smooth=None):
         super(SelectSmoothing, self).__init__(parent)
@@ -203,23 +257,26 @@ class SelectSmoothing(QMainWindow):
         self.parent = parent
         self.title = "Smoothing Selection"
 
-        self.data = data
+        self.data = data  # Glue data to be smoothed
+
+        # Check if smooth object is the caller
         if smooth is None:
             self.smooth = Smooth(data=self.data)
         else:
             self.smooth = smooth
 
-        self.abort_window = None
+        self.abort_window = None  # Small window pop up when smoothing.
 
-        self.component_id = None
-        self.current_axis = None
-        self.current_kernel_type = None
-        self.current_kernel_name = None
+        self.component_id = None  # Glue data component to smooth over
+        self.current_axis = None  # Selected smoothing_axis
+        self.current_kernel_type = None  # Selected kernel type, a key in Smooth.kernel_registry
+        self.current_kernel_name = None  # Name of selected kernel
 
-        self._init_selection_ui()
+        self._init_selection_ui() # Format and show gui
 
     def _init_selection_ui(self):
-        self.axes_prompt = QLabel("Smoothing Axes:")
+        # LINE 1: Radio box spatial vs spectral axis
+        self.axes_prompt = QLabel("Smoothing Axis:")
         self.axes_prompt.setMinimumWidth(150)
 
         self.spatial_radio = QRadioButton("Spatial")
@@ -236,9 +293,10 @@ class SelectSmoothing(QMainWindow):
         hbl1.addWidget(self.spatial_radio)
         hbl1.addWidget(self.spectral_radio)
 
+        # LINE 2: Kernel Type prompt
         self.k_type_prompt = QLabel("Kernel Type:")
         self.k_type_prompt.setMinimumWidth(150)
-
+        # Load kernel types + names and add to drop down
         self._load_options()
         self.combo = QComboBox()
         self.combo.setMinimumWidth(150)
@@ -248,6 +306,7 @@ class SelectSmoothing(QMainWindow):
         hbl2.addWidget(self.k_type_prompt)
         hbl2.addWidget(self.combo)
 
+        # LINE 3: Kernel size
         self.size_prompt = QLabel(self.smooth.get_kernel_size_prompt(self.current_kernel_type))
         self.size_prompt.setWordWrap(True)
         self.size_prompt.setMinimumWidth(150)
@@ -259,10 +318,11 @@ class SelectSmoothing(QMainWindow):
         hbl3.addWidget(self.k_size)
         hbl3.addWidget(self.unit_label)
 
+        # LINE 4: Data component drop down
         self.component_prompt = QLabel("Data Component:")
         self.component_prompt.setWordWrap(True)
         self.component_prompt.setMinimumWidth(150)
-
+        # Load component_ids and add to drop down
         component_ids = [str(i) for i in self.data.component_ids()]
         self.component_combo = QComboBox()
         self.component_combo.addItems(
@@ -277,6 +337,7 @@ class SelectSmoothing(QMainWindow):
         hbl4.addWidget(self.component_prompt)
         hbl4.addWidget(self.component_combo)
 
+        # LINE 5: ok cancel buttons
         self.okButton = QPushButton("OK")
         self.okButton.clicked.connect(self.call_main)
         self.okButton.setDefault(True)
@@ -289,6 +350,7 @@ class SelectSmoothing(QMainWindow):
         hbl5.addWidget(self.cancelButton)
         hbl5.addWidget(self.okButton)
 
+        # Add Lines to Vertical Layout
         # vbl is short for Vertical Box Layout
         vbl = QVBoxLayout()
         vbl.addLayout(hbl1)
@@ -297,18 +359,21 @@ class SelectSmoothing(QMainWindow):
         vbl.addLayout(hbl4)
         vbl.addLayout(hbl5)
 
+        # Add Vertical Layout to Central Widget
         self.wid = QWidget(self)
         self.setCentralWidget(self.wid)
         self.wid.setLayout(vbl)
 
         self.setMaximumWidth(330)
 
+        # Connect kernel combo box to event handler
         self.combo.currentIndexChanged.connect(self.selection_changed)
         self.selection_changed(0)
 
         self.show()
 
     def _load_options(self):
+        """Extract names + types of kernels from Smooth.kernel_registry"""
         kernel_registry = self.smooth.get_kernel_registry()
 
         self.options = {"spatial": [], "spectral": []}
@@ -326,6 +391,11 @@ class SelectSmoothing(QMainWindow):
         self.current_kernel_type = self.smooth.name_to_kernel_type(self.options[self.currentAxes][0])
 
     def init_abort_ui(self):
+        """
+        init abort or notification ui.
+        Displays while smoothing freezes the application.
+        Allows abort button to be added if needed.
+        """
         self.abort_window = QMainWindow(
             parent=self.parent,
             flags=Qt.WindowStaysOnTopHint)
@@ -334,6 +404,8 @@ class SelectSmoothing(QMainWindow):
 
         label_a_1 = QLabel("Executing smoothing algorithm.")
         label_a_2 = QLabel("This may take several minutes.")
+
+        # Add abort button here.
 
         # vbl is short for Vertical Box Layout
         vbl = QVBoxLayout()
@@ -349,7 +421,7 @@ class SelectSmoothing(QMainWindow):
     def selection_changed(self, i):
         """
         Update kernel type, units, etc... when
-        smoothing function selection changes.
+        kernel name changes in combo box.
         """
         keys = self.options[self.current_axis]
         name = keys[i]
@@ -369,13 +441,25 @@ class SelectSmoothing(QMainWindow):
         self.combo.addItems(self.options[self.current_axis])
 
     def input_validation(self):
+        """
+        Check if input will break Smoothing
+        :return: bool: True if no errors
+        """
+
+        # Check 1: k_size
         if self.k_size == "":
             self.k_size.setStyleSheet("background-color: rgba(255, 0, 0, 128);")
             return False
         else:
             try:
-                k_size = int(self.k_size.text())
+                if self.current_kernel_type == "median":
+                    k_size = int(self.k_size.text())
+                    self.k_size.setText("Must be int for median")
+                else:
+                    k_size = float(self.k_size.text())
             except ValueError:
+                if self.current_kernel_type == "median":
+                    self.k_size.setText("Must be int for median")
                 self.k_size.setStyleSheet("background-color: rgba(255, 0, 0, 128);")
                 return False
             if k_size <= 0:
@@ -394,7 +478,7 @@ class SelectSmoothing(QMainWindow):
 
     def main(self):
         """
-        Main function to process input and call smoothing.smooth.
+        Main function to process input and call smoothing.smooth_cube
         """
         success = self.input_validation()
 
