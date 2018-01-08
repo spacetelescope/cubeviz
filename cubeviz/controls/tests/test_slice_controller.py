e498c94e75c823b68a4b51b91b2d7a295d7b6306
Daniel D'Avella <drdavella@gmail.com>
2018-01-08 17:16:51 -0500

    Add unit test for out of bounds slice indices. Refactoring

--- cubeviz/controls/tests/test_slice_controller.py
+++ cubeviz/controls/tests/test_slice_controller.py
@@ -9,8 +9,22 @@ def enter_slice_text(qtbot, layout, text):
     widget.setText(text)
     qtbot.keyClick(widget, QtCore.Qt.Key_Enter)
 
+def assert_viewer_indices(layout, index):
+    for viewer in layout.all_views:
+        assert viewer._widget.slice_index == index
+
 @pytest.mark.parametrize('slice_index', [0, 100, 1000, 1024])
 def test_enter_valid_slice_text(qtbot, cubeviz_layout, slice_index):
     enter_slice_text(qtbot, cubeviz_layout, str(slice_index))
-    for viewer in cubeviz_layout.all_views:
-        assert viewer._widget.slice_index == slice_index
+    assert_viewer_indices(cubeviz_layout, slice_index)
+
+def test_enter_oob_slice_text(qtbot, cubeviz_layout):
+    # Enter a negative slice value
+    enter_slice_text(qtbot, cubeviz_layout, "-42")
+    assert_viewer_indices(cubeviz_layout, 0)
+
+    max_slice = len(cubeviz_layout._data['FLUX']) - 1
+
+    # Enter an impossibly large slice value
+    enter_slice_text(qtbot, cubeviz_layout, str(2**20))
+    assert_viewer_indices(cubeviz_layout, max_slice)
