9ec056da47f2d46fb8c61c5c35e7f81fe16f8323
Daniel D'Avella <drdavella@gmail.com>
2018-01-08 17:35:28 -0500

    Validate slice textbox contents when updating

--- cubeviz/controls/tests/test_slice_controller.py
+++ cubeviz/controls/tests/test_slice_controller.py
@@ -6,28 +6,34 @@ from qtpy import QtCore
 
 def enter_slice_text(qtbot, layout, text):
     widget = layout._slice_controller._slice_textbox
-    widget.setText(text)
+    widget.setText(str(text))
     qtbot.keyClick(widget, QtCore.Qt.Key_Enter)
 
 def assert_viewer_indices(layout, index):
     for viewer in layout.all_views:
         assert viewer._widget.slice_index == index
 
+def assert_slice_text(layout, text):
+    assert layout._slice_controller._slice_textbox.text() == str(text)
+
 @pytest.mark.parametrize('slice_index', [0, 100, 1000, 1024])
 def test_enter_valid_slice_text(qtbot, cubeviz_layout, slice_index):
-    enter_slice_text(qtbot, cubeviz_layout, str(slice_index))
+    enter_slice_text(qtbot, cubeviz_layout, slice_index)
     assert_viewer_indices(cubeviz_layout, slice_index)
+    assert_slice_text(cubeviz_layout, slice_index)
 
 def test_enter_oob_slice_text(qtbot, cubeviz_layout):
     # Enter a negative slice value
     enter_slice_text(qtbot, cubeviz_layout, "-42")
     assert_viewer_indices(cubeviz_layout, 0)
+    assert_slice_text(cubeviz_layout, 0)
 
     max_slice = len(cubeviz_layout._data['FLUX']) - 1
 
     # Enter an impossibly large slice value
     enter_slice_text(qtbot, cubeviz_layout, str(2**20))
     assert_viewer_indices(cubeviz_layout, max_slice)
+    assert_slice_text(cubeviz_layout, max_slice)
 
 @pytest.mark.parametrize('bad_text', ['garbage', '1e-07', '3.14', ''])
 def test_garbage_slice_text(qtbot, cubeviz_layout, bad_text):
@@ -35,7 +41,9 @@ def test_garbage_slice_text(qtbot, cubeviz_layout, bad_text):
     slice_index = 1024
     enter_slice_text(qtbot, cubeviz_layout, str(slice_index))
     assert_viewer_indices(cubeviz_layout, slice_index)
+    assert_slice_text(cubeviz_layout, slice_index)
 
     # Make sure that entering garbage text does not change the index
     enter_slice_text(qtbot, cubeviz_layout, bad_text)
     assert_viewer_indices(cubeviz_layout, slice_index)
+    assert_slice_text(cubeviz_layout, bad_text)
