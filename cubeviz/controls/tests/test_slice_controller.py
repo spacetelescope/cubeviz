02845b1caa6bdceccd1d12bbd25b65520a85fab2
Daniel D'Avella <ddavella@stsci.edu>
2018-01-12 08:59:02 -0500

    Refactor sync test function to make use of new test fixture

--- cubeviz/controls/tests/test_slice_controller.py
+++ cubeviz/controls/tests/test_slice_controller.py
@@ -125,42 +125,56 @@ def test_garbage_wavelength_text(qtbot, cubeviz_layout, bad_text):
     assert_all_viewer_indices(cubeviz_layout, slice_index)
     assert_wavelength_text(cubeviz_layout, bad_text)
 
-def unsync_and_verify(qtbot, layout, checkbox, viewer, other_viewers,
-                      synced_index, unsynced_index):
+def unsync_and_verify(qtbot, layout, sync_params, synced_index, unsynced_index):
+    unsynced_viewer = sync_params['unsynced_viewer']
+
     # Unsync the selected viewer
-    left_click(qtbot, checkbox)
+    left_click(qtbot, sync_params['checkbox'])
     # At this point no slice indices should have changed yet
     assert layout.synced_index == synced_index
     assert_all_viewer_indices(layout, synced_index)
 
     # Activate the unsynced viewer
-    select_viewer(qtbot, viewer)
+    select_viewer(qtbot, unsynced_viewer)
     # Change the slice index
     enter_slice_text(qtbot, layout, str(unsynced_index))
     # Make sure the active (unsynced) viewer's index has been updated
-    assert viewer._widget.slice_index == unsynced_index
+    assert unsynced_viewer._widget.slice_index == unsynced_index
     # Make sure no other viewer indices have been updated
-    assert_viewer_indices(other_viewers, synced_index)
+    assert_viewer_indices(sync_params['other_viewers'], synced_index)
     # Make sure the displayed slice index reflects the active viewer
     assert_slice_text(layout, str(unsynced_index))
 
+@pytest.fixture(scope='module')
+def sync_params(cubeviz_layout, request):
+    """This fixture expects indirect parametrization providing the viewer index"""
+    viewer_index = request.param
+
+    params = dict(
+        checkbox=cubeviz_layout._synced_checkboxes[viewer_index],
+        unsynced_viewer=cubeviz_layout.all_views[viewer_index],
+        other_viewers=all_but_index(cubeviz_layout.all_views, viewer_index),
+        synced_viewer=all_but_index(
+            cubeviz_layout.split_views, viewer_index-1)[0]
+    )
+
+    return params
+
 # The single cube viewer will be tested by a separate test case
-@pytest.mark.parametrize('viewer_index', [1, 2, 3])
-def test_unsync_viewer(qtbot, cubeviz_layout, viewer_index):
+@pytest.mark.parametrize('sync_params', [1, 2, 3], indirect=True)
+def test_unsync_viewer(qtbot, cubeviz_layout, sync_params):
     # Get the current index of all viewers
     synced_index = cubeviz_layout.synced_index
     # Sanity check to make sure all viewers are actually synced
     assert_all_viewer_indices(cubeviz_layout, synced_index)
 
-    checkbox = cubeviz_layout._synced_checkboxes[viewer_index]
-    unsynced_viewer = cubeviz_layout.all_views[viewer_index]
-    other_viewers = all_but_index(cubeviz_layout.all_views, viewer_index)
+    unsynced_viewer = sync_params['unsynced_viewer']
+    other_viewers = sync_params['other_viewers']
 
     # Activate the unsynced viewer
     unsynced_index = 42
     unsync_and_verify(
-        qtbot, cubeviz_layout, checkbox, unsynced_viewer, other_viewers,
-        synced_index, unsynced_index)
+        qtbot, cubeviz_layout, sync_params, synced_index, unsynced_index)
 
     # Toggle to single image mode
     toggle_viewer(qtbot, cubeviz_layout)
@@ -171,8 +185,7 @@ def test_unsync_viewer(qtbot, cubeviz_layout, viewer_index):
     assert_slice_text(cubeviz_layout, str(unsynced_index))
 
     # Now activate one of the synced viewers
-    synced_viewer = all_but_index(cubeviz_layout.split_views, viewer_index-1)[0]
-    select_viewer(qtbot, synced_viewer)
+    select_viewer(qtbot, sync_params['synced_viewer'])
     assert_slice_text(cubeviz_layout, str(synced_index))
     # Make sure that no indices have changed
     assert unsynced_viewer._widget.slice_index == unsynced_index
@@ -190,27 +203,27 @@ def test_unsync_viewer(qtbot, cubeviz_layout, viewer_index):
     assert_slice_text(cubeviz_layout, str(unsynced_index))
 
     # Sync the unsynced viewer
-    left_click(qtbot, checkbox)
+    left_click(qtbot, sync_params['checkbox'])
     assert_all_viewer_indices(cubeviz_layout, synced_index)
     assert_slice_text(cubeviz_layout, str(synced_index))
 
-@pytest.mark.parametrize('viewer_index', [1, 2, 3])
-def test_resync_inactive_viewer(qtbot, cubeviz_layout, viewer_index):
+@pytest.mark.parametrize('sync_params', [1, 2, 3], indirect=True)
+def test_resync_inactive_viewer(qtbot, cubeviz_layout, sync_params):
+    """In contrast to the previous test, this test makes sure the unsynced
+    viewer is successfully re-synced even when it is not the active viewer.
+    """
     # Get the current index of all viewers
     synced_index = cubeviz_layout.synced_index
 
-    checkbox = cubeviz_layout._synced_checkboxes[viewer_index]
-    unsynced_viewer = cubeviz_layout.all_views[viewer_index]
-    other_viewers = all_but_index(cubeviz_layout.all_views, viewer_index)
+    unsynced_viewer = sync_params['unsynced_viewer']
+    other_viewers = sync_params['other_viewers']
 
     unsynced_index = 42
     unsync_and_verify(
-        qtbot, cubeviz_layout, checkbox, unsynced_viewer, other_viewers,
-        synced_index, unsynced_index)
+        qtbot, cubeviz_layout, sync_params, synced_index, unsynced_index)
 
     # Now activate one of the synced viewers
-    synced_viewer = all_but_index(cubeviz_layout.split_views, viewer_index-1)[0]
-    select_viewer(qtbot, synced_viewer)
+    select_viewer(qtbot, sync_params['synced_viewer'])
     assert_slice_text(cubeviz_layout, str(synced_index))
     # Make sure that no indices have changed
     assert unsynced_viewer._widget.slice_index == unsynced_index
@@ -224,7 +237,7 @@ def test_resync_inactive_viewer(qtbot, cubeviz_layout, viewer_index):
     assert_slice_text(cubeviz_layout, str(synced_index))
 
     # Sync the (inactive) unsynced viewer
-    left_click(qtbot, checkbox)
+    left_click(qtbot, sync_params['checkbox'])
     assert_all_viewer_indices(cubeviz_layout, synced_index)
     assert_slice_text(cubeviz_layout, str(synced_index))
 
