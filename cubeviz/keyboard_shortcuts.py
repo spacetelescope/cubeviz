93d6da2d59aa027a77c290558e322db7c335be5f
Craig Jones <crjones@stsci.edu>
2018-03-08 11:45:21 -0500

    added in keystroke for bringing up the wavelenght GUI

--- cubeviz/keyboard_shortcuts.py
+++ cubeviz/keyboard_shortcuts.py
@@ -1,6 +1,7 @@
 from glue.config import keyboard_shortcut
 from qtpy import QtCore, QtWidgets, QtGui, compat
 
+from .controls.units_ui import WavelengthUI
 
 @keyboard_shortcut(QtCore.Qt.Key_Left, None)
 @keyboard_shortcut(QtCore.Qt.Key_A, None)
@@ -15,7 +16,7 @@ def move_slider_left(session):
 
 
 @keyboard_shortcut(QtCore.Qt.Key_Right, None)
-@keyboard_shortcut(QtCore.Qt.Key_D, None)
+#@keyboard_shortcut(QtCore.Qt.Key_D, None)
 def move_slider_right(session):
     """
     Move slider index one to the right
@@ -24,3 +25,14 @@ def move_slider_right(session):
     """
     curr_layout = session.application.current_tab.ui
     curr_layout.change_slice_index(1)
+
+@keyboard_shortcut(QtCore.Qt.Key_1, None)
+def wavelengthui(session):
+    """
+    Bring up the wavelength UI in order to change the wavelength units or redshift
+
+    :param session:
+    :return:
+    """
+    cubevizlayout = session.application.tab(0)
+    WavelengthUI(cubevizlayout._units_controller.unit_titles, parent=cubevizlayout)
