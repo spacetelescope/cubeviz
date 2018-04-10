f11b4be51a397b546e337290e39b61dc5b822b5f
Daniel D'Avella <ddavella@stsci.edu>
2018-04-10 13:44:52 -0400

    Add basic test for spatial ROI deletion in specviz

--- cubeviz/tests/test_specviz_interaction.py
+++ cubeviz/tests/test_specviz_interaction.py
@@ -15,6 +15,23 @@ def test_create_roi(cubeviz_layout):
     subset = cubeviz_layout._data.subsets[0]
     specviz = cubeviz_layout.specviz._widget
     assert subset in specviz._specviz_data_cache
+    assert len(specviz._specviz_data_cache) == 2
 
     layer = specviz._specviz_data_cache[subset]
     assert layer in specviz._layer_list.all_layers
+    # TODO: once this is fixed, there should be a test here to make sure the
+    # ROI is the only spectrum that is currently displayed
+
+def test_delete_roi(cubeviz_layout):
+    # This test assumes that an ROI was created by the previous test
+    dc = cubeviz_layout.session.application.data_collection
+    dc.remove_subset_group(dc.subset_groups[0])
+    assert len(cubeviz_layout._data.subsets) == 0
+
+    specviz = cubeviz_layout.specviz._widget
+    assert cubeviz_layout._data in specviz._specviz_data_cache
+    assert len(specviz._specviz_data_cache) == 1
+
+    layer = specviz._specviz_data_cache[cubeviz_layout._data]
+    assert layer in specviz._layer_list.all_layers
+    assert len(specviz._layer_list.all_layers) == 1
