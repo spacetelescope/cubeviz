6d1daf3934ae0e7dadcce0f2b3bb836a9d05bcf2
Jesse Averbukh <javerbukh@stsci.edu>
2018-04-13 12:50:34 -0400

    test moment maps 2 works as well

--- cubeviz/tools/tests/test_moment_map.py
+++ cubeviz/tools/tests/test_moment_map.py
@@ -29,7 +29,7 @@ def assert_red_stylesheet(widget):
     assert widget.styleSheet() == "color: rgba(255, 0, 0, 128)"
 
 
-def test_moment_display(qtbot, cubeviz_layout):
+def test_moment_maps_1(cubeviz_layout):
     # Create GUI
     mm = moment_maps(cubeviz_layout)
     mm.display()
@@ -56,3 +56,30 @@ def test_moment_display(qtbot, cubeviz_layout):
 
     assert np.allclose(cube_moment, np_result, atol=1.0, equal_nan=True)
 
+
+def test_moment_maps_2(cubeviz_layout):
+    # Create GUI
+    mm = moment_maps(cubeviz_layout)
+    mm.display()
+    mm.order_combobox.setCurrentIndex(1)
+    mm.data_combobox.setCurrentIndex(0)
+
+    # Call calculate function and get result
+    mm.calculate_callback()
+    moment_component_id = [str(x) for x in cubeviz_layout._data.container_2d.component_ids() if str(x).startswith('018.DATA-mom')][0]
+    np_result = cubeviz_layout._data.container_2d[moment_component_id]
+
+    # Expected result
+    np_data = cubeviz_layout._data[DATA_LABELS[0]]
+    import spectral_cube
+    cube = spectral_cube.SpectralCube(np_data, wcs=cubeviz_layout._data.coords.wcs)
+    order = int(mm.order_combobox.currentText())
+    cube_moment = np.asarray(cube.moment(order=order, axis=0))
+
+    # Visually check differences
+    print('np_data_sum {}'.format(cube_moment, type(cube_moment)))
+    print('np_result {}, {}'.format(np_result, type(np_result)))
+    # print('sum', np.sum(np.abs(cube_moment - np_result)))
+    # print('how many nans in cube_moment', np.sum(not np.isfinite(cube_moment.all())))
+
+    assert np.allclose(cube_moment, np_result, atol=1.0, equal_nan=True)
