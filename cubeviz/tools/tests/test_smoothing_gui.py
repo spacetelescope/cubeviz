c1b66d85cbe36e7262fefbdec1343e187681532f
Jesse Averbukh <javerbukh@stsci.edu>
2018-04-17 12:15:31 -0400

    starting smoothing tests

--- cubeviz/tools/tests/test_smoothing_gui.py
+++ cubeviz/tools/tests/test_smoothing_gui.py
@@ -6,7 +6,7 @@ import numpy as np
 
 from qtpy import QtCore
 from glue.core import roi
-from cubeviz.tools.moment_maps import MomentMapsGUI
+from cubeviz.tools.smoothing import SelectSmoothing
 
 from ...tests.helpers import (toggle_viewer, select_viewer, left_click,
                       left_button_press, right_button_press, enter_slice_text,
@@ -17,12 +17,12 @@ DATA_LABELS = ['018.DATA', '018.NOISE']
 
 
 @pytest.fixture(scope='module')
-def moment_maps(cubeviz_layout):
+def smoothing(cubeviz_layout):
     cl = cubeviz_layout
 
-    mm = MomentMapsGUI(cl._data, cl.session.data_collection, parent=cl)
+    sm = SelectSmoothing(cl._data, parent=cl)
 
-    return mm
+    return sm
 
 
 def assert_red_stylesheet(widget):
@@ -31,13 +31,13 @@ def assert_red_stylesheet(widget):
 
 def test_moment_maps_1(cubeviz_layout):
     # Create GUI
-    mm = moment_maps(cubeviz_layout)
-    mm.display()
-    mm.order_combobox.setCurrentIndex(0)
-    mm.data_combobox.setCurrentIndex(0)
+    sm = smoothing(cubeviz_layout)
+    sm.k_size.setText("1")
+    sm.combo.setCurrentIndex(0)
+    sm.component_combo.setCurrentIndex(0)
 
     # Call calculate function and get result
-    mm.calculate_callback()
+    sm.call_main()
     moment_component_id = [str(x) for x in cubeviz_layout._data.container_2d.component_ids() if str(x).startswith('018.DATA-moment-1')][0]
     np_result = cubeviz_layout._data.container_2d[moment_component_id]
 
@@ -45,29 +45,7 @@ def test_moment_maps_1(cubeviz_layout):
     np_data = cubeviz_layout._data[DATA_LABELS[0]]
     import spectral_cube
     cube = spectral_cube.SpectralCube(np_data, wcs=cubeviz_layout._data.coords.wcs)
-    order = int(mm.order_combobox.currentText())
-    cube_moment = np.asarray(cube.moment(order=order, axis=0))
-
-    assert np.allclose(cube_moment, np_result, atol=1.0, equal_nan=True)
-
-
-def test_moment_maps_2(cubeviz_layout):
-    # Create GUI
-    mm = moment_maps(cubeviz_layout)
-    mm.display()
-    mm.order_combobox.setCurrentIndex(1)
-    mm.data_combobox.setCurrentIndex(0)
-
-    # Call calculate function and get result
-    mm.calculate_callback()
-    moment_component_id = [str(x) for x in cubeviz_layout._data.container_2d.component_ids() if str(x).startswith('018.DATA-moment-2')][0]
-    np_result = cubeviz_layout._data.container_2d[moment_component_id]
-
-    # Expected result
-    np_data = cubeviz_layout._data[DATA_LABELS[0]]
-    import spectral_cube
-    cube = spectral_cube.SpectralCube(np_data, wcs=cubeviz_layout._data.coords.wcs)
-    order = int(mm.order_combobox.currentText())
+    order = int(sm.order_combobox.currentText())
     cube_moment = np.asarray(cube.moment(order=order, axis=0))
 
     assert np.allclose(cube_moment, np_result, atol=1.0, equal_nan=True)
