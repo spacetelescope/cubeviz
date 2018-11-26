5cb629df23b0082c8ec8dad0a926826eb4c230ef
Craig Jones <craig@brechmos.org>
2018-11-26 10:41:00 -0500

    dependency version fix, cubeviz dev version fix

--- cubeviz/__init__.py
+++ cubeviz/__init__.py
@@ -4,14 +4,6 @@
 This is an Astropy affiliated package.
 """
 
-import astropy.units as units
-from .flux_equivalences import CustomFluxEquivalences
-# We override the units.equivalencies.spectral_density function with
-# CustomFluxEquivalences before the program starts. We expect all libraries
-# to access CustomFluxEquivalences when calling for units.equivalencies.spectral_density
-units.equivalencies.spectral_density = CustomFluxEquivalences(units.equivalencies.spectral_density)
-units.spectral_density = units.equivalencies.spectral_density
-
 # Affiliated packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
@@ -19,4 +11,12 @@ from ._internal_init import *
 # ----------------------------------------------------------------------------
 
 if _CUBEVIZ_SETUP_ is False:
+    import astropy.units as units
+    from .flux_equivalences import CustomFluxEquivalences
+    # We override the units.equivalencies.spectral_density function with
+    # CustomFluxEquivalences before the program starts. We expect all libraries
+    # to access CustomFluxEquivalences when calling for units.equivalencies.spectral_density
+    units.equivalencies.spectral_density = CustomFluxEquivalences(units.equivalencies.spectral_density)
+    units.spectral_density = units.equivalencies.spectral_density
+
     from . import keyboard_shortcuts
