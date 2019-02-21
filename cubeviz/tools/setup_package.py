d3744722d72027a2866c559652da48a0654b1925
Daniel D'Avella <ddavella@stsci.edu>
2019-02-21 14:37:53 -0500

    Remove use of _ASTROPY_PACKAGE_NAME_ and setup_package.py files

--- cubeviz/tools/setup_package.py
+++
@@ -1,3 +0,0 @@
-def get_package_data():
-    return {
-        _ASTROPY_PACKAGE_NAME_ + '.tools': ['wavelength.ui', 'collapse.ui']}
--- setup.py
+++ setup.py
@@ -29,10 +29,6 @@ metadata = dict(conf.items('metadata'))
 
 PACKAGENAME = metadata.get('package_name', 'cubeviz')
 
-# Store the package name in a built-in variable so it's easy
-# to get from other parts of the setup infrastructure
-builtins._ASTROPY_PACKAGE_NAME_ = PACKAGENAME
-
 # VERSION should be PEP440 compatible (http://www.python.org/dev/peps/pep-0440)
 VERSION = metadata.get('version', '0.0.dev0')
 
@@ -51,26 +47,9 @@ cmdclassd = register_commands(PACKAGENAME, VERSION, RELEASE)
 generate_version_py(PACKAGENAME, VERSION, RELEASE,
                     get_debug_option(PACKAGENAME))
 
-# Get configuration information from all of the various subpackages.
-# See the docstring for setup_helpers.update_package_files for more
-# details.
-package_info = get_package_info()
-
-# Add the project-global data
-package_info['package_data'].setdefault(PACKAGENAME, [])
-package_info['package_data'][PACKAGENAME].append('data/*')
-package_info['package_data'][PACKAGENAME].append('data/ui/*')
-package_info['package_data'][PACKAGENAME].append('data/resources/*')
-package_info['package_data'][PACKAGENAME].append('controls/*yaml')
-
-# Note that requires and provides should not be included in the call to
-# ``setup``, since these are now deprecated. See this link for more details:
-# https://groups.google.com/forum/#!topic/astropy-dev/urYO8ckB2uM
-
 setup(name=PACKAGENAME,
       version=VERSION,
       cmdclass=cmdclassd,
       zip_safe=False,
       use_2to3=False,
-      **package_info
 )
