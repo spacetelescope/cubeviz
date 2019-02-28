adf7a5936305dcd2a19f3b4c23ff11c9a01c4432
Daniel D'Avella <ddavella@stsci.edu>
2019-02-28 09:20:35 -0500

    Explicitly set mpl backend to PyQt5...
    This prevents the "python must be installed as a framework" error that
    commonly occurs on OSX. This means we no longer need to install
    matplotlib using conda in our test environments.

--- cubeviz/__init__.py
+++ cubeviz/__init__.py
@@ -19,6 +19,12 @@ except DistributionNotFound:
     __version__ = "unknown"
 
 
+# This *must* occur before any imports from glue. It prevents the "python must
+# be installed as a framework" error that occurs on OSX. Since we now
+# explicitly depend on PyQt5, we use it as the mpl backend.
+import matplotlib as mpl
+mpl.use('Qt5Agg')
+
 import astropy.units as units
 from .flux_equivalences import CustomFluxEquivalences
 # We override the units.equivalencies.spectral_density function with
